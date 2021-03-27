338bf5ba0c7b5b4a348877975bb603a80db0dcf7
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-03-28 00:55:37 +0200

    feat: change mesh show method

--- mesh.py
+++ mesh.py
@@ -1,6 +1,5 @@
-#Some info about numpy-stl: https://pypi.org/project/numpy-stl/
-import os
 import stl
+import numpy as np
 from matplotlib import pyplot
 from mpl_toolkits import mplot3d
 
@@ -9,13 +8,26 @@ class Mesh:
 
     def __init__(self, filepath: str) -> None:
         self.mesh = stl.mesh.Mesh.from_file(filepath)
+        self.vectors = self.mesh.vectors
+        self.x = self.vectors[:, :, 0].flatten()
+        self.y = self.vectors[:, :, 1].flatten()
+        self.z = self.vectors[:, :, 2].flatten()
 
     def show(self):
         figure = pyplot.figure()
         axes = mplot3d.Axes3D(figure)
         axes.add_collection3d(
-            mplot3d.art3d.Poly3DCollection(self.mesh.vectors)
+            mplot3d.art3d.Poly3DCollection(
+                self.vectors, 
+                facecolors='w', 
+                edgecolors='k',
+                linewidths=1,
+                alpha=0.5
+            )
         )
-        scale = self.mesh.points.flatten()
-        axes.auto_scale_xyz(scale, scale, scale)
+        axes.auto_scale_xyz(
+            [np.min(self.x), np.max(self.x)], 
+            [np.min(self.y), np.max(self.y)], 
+            [np.min(self.z), np.max(self.z)]
+        )
         pyplot.show()
